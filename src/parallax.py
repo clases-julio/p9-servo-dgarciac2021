7807e62959f44de2880b59ed9605b5963fb839f2
Diego Garcia Curras <d.garciac.2021@alumnos.urjc.es>
2022-12-22 19:37:45 +0100

    Push to RPi

--- src/parallax.py
+++ src/parallax.py
@@ -20,12 +20,23 @@ import RPi.GPIO as GPIO
 
 class Parallax:
 
-    class DirOfRot(Enum):
+    class __DirOfRot(Enum):
         CLOCKWISE = 0
         COUNTER_CLOCKWISE = 1
 
-    CLOCKWISE = DirOfRot.CLOCKWISE
-    COUNTER_CLOCKWISE = DirOfRot.COUNTER_CLOCKWISE
+    CLOCKWISE = __DirOfRot.CLOCKWISE
+    COUNTER_CLOCKWISE = __DirOfRot.COUNTER_CLOCKWISE
+
+    __PWM_FREQUENCY = 50
+    __PWM_PERIOD = 1/__PWM_FREQUENCY
+
+    __MAX_CW_PW = 1280
+    __MAX_ST_PW = 1520
+    __MAX_CCW_PW = 1720
+
+    __MIN_CW_PW = 1479
+    __MIN_ST_PW = 1480
+    __MIN_CCW_PW = 1521
 
     def __init__(self, cPin, fPin):
         GPIO.setmode(GPIO.BCM)
@@ -38,8 +49,29 @@ class Parallax:
         GPIO.setup(self.controlPin, GPIO.OUT)
         GPIO.setup(self.feedbackPin, GPIO.IN)
 
-        self.__servo = GPIO.PWM(self.controlPin, 50) 
-        self.__servo.start(0)
+        self.__servo = GPIO.PWM(self.controlPin, self.__PWM_FREQUENCY) 
+        self.__servo.ChangeDutyCycle(0)
+    
+    def __del__(self):
+        self.__servo.ChangeDutyCycle(0)
+    
+    def __calculateDutyCycle(self, pulseWidth):
+        return (pulseWidth/self.__PWM_PERIOD) * 100.0
+
+    
+    def __linearInterpolate(self):
+        if(self.turnDirection is self.CLOCKWISE):
+            max = self.__MAX_CW_PW
+            min = self.__MIN_CW_PW
+        if(self.turnDirection is self.COUNTER_CLOCKWISE):
+            max = self.__MAX_CCW_PW
+            min = self.__MIN_CCW_PW
+
+    def setRotationDir(self, rotationDir):
+        self.rotationDirection = rotationDir
 
     def run(self):
-        self.__servo.ChangeDutyCycle(8.6)
+        self.__servo.ChangeDutyCycle(self.__calculateDutyCycle(self.__MAX_CCW_PW))
+    
+    def stop(self):
+        self.__servo.ChangeDutyCycle(0)
