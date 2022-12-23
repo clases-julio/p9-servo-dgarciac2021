141803f7e82b34c36a04b504ec45d477b0b06679
Diego Garcia Curras <d.garciac.2021@alumnos.urjc.es>
2022-12-23 15:29:07 +0100

    Push to RPI

--- src/parallax.py
+++ src/parallax.py
@@ -50,10 +50,12 @@ class Parallax:
         self.__feedbackReader = read_PWM.reader(self.__pi, self.feedbackPin)
 
     def __del__(self):
+        print(self.__pi)
         if self.__pi:
             self.destroy()
 
     def destroy(self):
+        print("Destroyer called")
         self.__pi.set_servo_pulsewidth(self.controlPin, 0)
         self.__feedbackReader.cancel()
         self.__pi.stop()
