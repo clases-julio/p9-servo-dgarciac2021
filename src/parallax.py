1783588c57ed6793a0938e843a7e00f5a0215cca
Diego Garcia Curras <d.garciac.2021@alumnos.urjc.es>
2022-12-24 20:44:02 +0100

    Push to RPI

--- src/parallax.py
+++ src/parallax.py
@@ -118,6 +118,8 @@ class Parallax:
         for sample_list in feedback_samples:
             print("Pulse width: ", sample_list[0], end="  |  ")
             print("Samples gathered: ", len(sample_list), end="\n")
+            if sample_list[0] == 1500:
+                print(sample_list)
 
 
     def stop(self):
