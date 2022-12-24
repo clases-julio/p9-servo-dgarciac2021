6f86d180159bf0c89a1abcb1251bf247306073e2
Diego Garcia Curras <d.garciac.2021@alumnos.urjc.es>
2022-12-24 20:05:19 +0100

    Push to RPI

--- src/parallax.py
+++ src/parallax.py
@@ -97,6 +97,7 @@ class Parallax:
         while pw <= max_pw:
             if pw is not self.__pi.get_servo_pulsewidth(self.controlPin):
                 self.__pi.set_servo_pulsewidth(self.controlPin, pw)
+                continue
             
             print(self.__feedbackReader.duty_cycle())
             #feedback_samples.append(self.__feedbackReader.duty_cycle())
