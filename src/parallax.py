d35046b7c5fb0c744a4bdf5495aaf44f431cfd2d
Diego Garcia Curras <d.garciac.2021@alumnos.urjc.es>
2022-12-24 21:15:17 +0100

    Push to RPI

--- src/parallax.py
+++ src/parallax.py
@@ -99,7 +99,7 @@ class Parallax:
         while time.time() - time_milestone < 10.0:
             feedback_sample = round(self.__feedbackReader.duty_cycle(), 2)
             if feedback_sample != 0.0:
-                if feedback_sample < 20.0 or feedback_sample > 90.0:
+                if feedback_sample < 25.0 or feedback_sample > 85.0:
                     self.__pi.set_servo_pulsewidth(self.controlPin, 1450)
                 else:
                     self.__pi.set_servo_pulsewidth(self.controlPin, pw)
