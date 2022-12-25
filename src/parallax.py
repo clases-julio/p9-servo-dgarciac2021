9901031d8e6954dda0377148b3ca046fc275a525
Diego Garcia Curras <d.garciac.2021@alumnos.urjc.es>
2022-12-25 16:34:38 +0100

    Push to RPI

--- src/parallax.py
+++ src/parallax.py
@@ -95,7 +95,7 @@ class Parallax:
     def __getFeedbackDCBounds(self):
         quick_pw = 1000
         slow_pw = 1450
-        test_timeout = 3.0
+        test_timeout = 15.0
         lower_dc_bound = 30.0
         upper_dc_bound = 80.0
         min_dc = 100.0
@@ -194,8 +194,8 @@ class Parallax:
         max_factor = 1.0 + factor/100
         min_factor = 1.0 - factor/100
 
-        self.__find_duty_cycle_boundaries(self.__MAX_CW_PW*min_factor, self.__MAX_CW_PW*max_factor)
-        self.__find_duty_cycle_boundaries(self.__MIN_CW_PW*min_factor, self.__MIN_CW_PW*max_factor)
+        self.__find_duty_cycle_boundaries(self.__MAX_CW_PW*min_factor, self.__MAX_CW_PW*max_factor, min_fb_dc, max_fb_dc)
+        self.__find_duty_cycle_boundaries(self.__MIN_CW_PW*min_factor, self.__MIN_CW_PW*max_factor, min_fb_dc, max_fb_dc)
         #self.__find_duty_cycle_boundaries(self.__MIN_CCW_PW*min_factor, self.__MIN_CCW_PW*max_factor)
         #self.__find_duty_cycle_boundaries(self.__MAX_CCW_PW*min_factor, self.__MAX_CCW_PW*max_factor)
 
