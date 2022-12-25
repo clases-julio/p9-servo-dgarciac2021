d53f0ab7fde936acc1feac066668dee39f6fd758
Diego Garcia Curras <d.garciac.2021@alumnos.urjc.es>
2022-12-25 17:31:17 +0100

    Push to RPI

--- src/parallax.py
+++ src/parallax.py
@@ -206,7 +206,7 @@ class Parallax:
 
         #self.__find_duty_cycle_boundaries(self.__MAX_CW_PW, self.__MAX_CW_PW*min_factor, self.__MAX_CW_PW*max_factor, min_fb_dc, max_fb_dc)
         print(self.__find_duty_cycle_boundaries(self.__MIN_CW_PW, self.__MIN_CW_PW*min_factor, self.__MIN_CW_PW*max_factor, min_fb_dc, max_fb_dc))
-        print(self.__find_duty_cycle_boundaries(self.__MIN_CCW_PW*min_factor, self.__MIN_CCW_PW*max_factor))
+        print(self.__find_duty_cycle_boundaries(self.__MIN_CCW_PW, self.__MIN_CCW_PW*min_factor, self.__MIN_CCW_PW*max_factor))
         #self.__find_duty_cycle_boundaries(self.__MAX_CCW_PW*min_factor, self.__MAX_CCW_PW*max_factor)
 
         print("Pulse width boundaries found!", end="\n\n")
