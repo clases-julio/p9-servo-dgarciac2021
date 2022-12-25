aac319cb43f79fdd03585a9a30c529b3666bf862
Diego Garcia Curras <d.garciac.2021@alumnos.urjc.es>
2022-12-25 16:59:42 +0100

    Push to RPI

--- src/parallax.py
+++ src/parallax.py
@@ -199,7 +199,7 @@ class Parallax:
         max_factor = 1.0 + factor/100
         min_factor = 1.0 - factor/100
 
-        #self.__find_duty_cycle_boundaries(self.__MAX_CW_PW*min_factor, self.__MAX_CW_PW*max_factor, min_fb_dc, max_fb_dc)
+        self.__find_duty_cycle_boundaries(self.__MAX_CW_PW*min_factor, self.__MAX_CW_PW*max_factor, min_fb_dc, max_fb_dc)
         self.__find_duty_cycle_boundaries(self.__MIN_CW_PW*min_factor, self.__MIN_CW_PW*max_factor, min_fb_dc, max_fb_dc)
         #self.__find_duty_cycle_boundaries(self.__MIN_CCW_PW*min_factor, self.__MIN_CCW_PW*max_factor)
         #self.__find_duty_cycle_boundaries(self.__MAX_CCW_PW*min_factor, self.__MAX_CCW_PW*max_factor)
