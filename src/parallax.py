c4a1d5601160ac4a6ea6266afe3f9fd7bcc30700
Diego Garcia Curras <d.garciac.2021@alumnos.urjc.es>
2022-12-28 20:54:12 +0100

    Push to RPI

--- src/parallax.py
+++ src/parallax.py
@@ -233,7 +233,7 @@ class Parallax:
         limit_feedback_samples = []
         lap_completed = False
 
-        while average_lap_time <= average_lap_time_max_speed * 1.025:
+        while average_lap_time <= average_lap_time_max_speed + 0.01:
             self.__run_and_wait(pulse_width)
 
             if lap_completed is False and self.getFeedbackDutyCycle() >= median_feedback_duty_cycle:
