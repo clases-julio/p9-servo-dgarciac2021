8ce85365650f4ef00545d6a49d73070579988ae1
Diego Garcia Curras <d.garciac.2021@alumnos.urjc.es>
2023-01-03 23:59:27 +0100

    Push to RPI

--- src/parallax.py
+++ src/parallax.py
@@ -109,12 +109,12 @@ class Parallax:
         if(self.rotation_direction is self.CLOCKWISE):
             max = self.__max_cw_pw
             min = self.__min_cw_pw
+            print(round(interp(power,[1,100],[min,max])))
             return round(interp(power,[1,100],[min,max]))
         elif(self.rotation_direction is self.COUNTER_CLOCKWISE):
             max = self.__max_ccw_pw
             min = self.__min_ccw_pw
-            valueScaled = float(power - 1) / float(99)
-            print(min + (valueScaled * (max - min)))
+            print(round(interp(power,[1,100],[min,max])))
             return round(interp(power,[1,100],[min,max]))
 
         # Linear approximation. According to pigpio, pulse width should be between 500-2500 μs, thus the round.
